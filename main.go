03f79c18122739d97f3521a65ec8cb2afc9be0e9
Xiro <xiro@duck.com>
2023-10-29 22:34:26 +0100

    feat: Added logger & custom 404 handler from fiber docs

--- main.go
+++ main.go
@@ -1,12 +1,21 @@
 package main
 
-import "github.com/gofiber/fiber/v2"
+import (
+	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/logger"
+)
 
 func main() {
 	app := fiber.New()
 
+	app.Use(logger.New())
+
 	app.Get("/", func(c *fiber.Ctx) error {
 		return c.SendString("Hello, World!")
+	})
+
+	app.Use(func(c *fiber.Ctx) error {
+		return c.Status(fiber.StatusNotFound).SendString("Sorry can't find that!")
 	})
 
 	app.Listen(":3000")
