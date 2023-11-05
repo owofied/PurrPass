8877119503ea6c69c620579f7c489c9e29b76a6f
Xiro <xiro@duck.com>
2023-11-05 16:43:30 +0100

    feat: Improved 404 error

--- main.go
+++ main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"fmt"
+
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/middleware/logger"
 )
@@ -15,7 +17,11 @@ func main() {
 	})
 
 	app.Use(func(c *fiber.Ctx) error {
-		return c.Status(fiber.StatusNotFound).SendString("Sorry can't find that!")
+		return c.Status(fiber.StatusNotFound).JSON(fiber.Map{
+			"statusCode": fiber.StatusNotFound,
+			"error":      "Not Found",
+			"message":    fmt.Sprintf("Cannot %s %s", c.Method(), c.Path()),
+		})
 	})
 
 	app.Listen(":3000")
