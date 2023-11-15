661d3acbafc9a4cd8d599d038a29aaeff175afaf
Xiro <xiro@duck.com>
2023-11-15 13:24:24 +0100

    refactor(db): Remove timezone option from postgres connection string

--- database/database.go
+++ database/database.go
@@ -8,7 +8,7 @@ import (
 var Conn *gorm.DB
 
 func ConnectPostgres() {
-	dsn := "host=localhost user=postgres password=postgres dbname=postgres port=5432 sslmode=disable TimeZone=Asia/Shanghai"
+	dsn := "host=localhost user=postgres password=postgres dbname=postgres port=5432 sslmode=disable"
 	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
 	if err != nil {
 		panic(err)
