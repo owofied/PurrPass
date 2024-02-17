8ddb65df1c05d19b7201e4eff7d01d1e10fe05b4
Xiro <xiro@duck.com>
2024-02-18 00:59:53 +0100

    feat: json config instead of env variables

--- cmd/main/main.go
+++ cmd/main/main.go
@@ -1,14 +1,30 @@
 package main
 
 import (
+	"errors"
+	"os"
+
 	"github.com/labstack/echo/v4"
+
+	"github.com/owofied/PurrPass/internal/config"
 	"github.com/owofied/PurrPass/internal/router"
 )
 
 func main() {
 	e := echo.New()
 
+	confDir, err := os.UserConfigDir()
+	if err != nil {
+		panic(err)
+	}
+
+	if _, err = os.Stat(confDir + "/PurrPass/config.json"); errors.Is(err, os.ErrNotExist) {
+		config.InitializeConfig()
+	}
+
+	conf := config.GetConfig()
+
 	router.SetupRouter(e)
 
-	e.Logger.Fatal(e.Start(":3000"))
+	e.Logger.Fatal(e.Start(":" + conf.Server.Port))
 }
