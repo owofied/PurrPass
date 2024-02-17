f43294408c958537e4d066c436a433943851bbad
Xiro <xiro@duck.com>
2024-02-17 14:25:38 +0100

    refactor: Temporarily remove database functions

--- database/database.go
+++ database/database.go
@@ -1,37 +1,37 @@
 package database
 
-import (
-	"fmt"
-	"os"
+// import (
+// 	"fmt"
+// 	"os"
 
-	"gorm.io/driver/postgres"
-	"gorm.io/gorm"
-)
+// 	"gorm.io/driver/postgres"
+// 	"gorm.io/gorm"
+// )
 
-var Conn *gorm.DB
+// var Conn *gorm.DB
 
-type Config struct {
-	Host     string
-	Port     string
-	User     string
-	Password string
-	DbName   string
-}
+// type Config struct {
+// 	Host     string
+// 	Port     string
+// 	User     string
+// 	Password string
+// 	DbName   string
+// }
 
-func ConnectPostgres() {
-	config := Config{
-		Host:     os.Getenv("POSTGRES_HOST"),
-		Port:     os.Getenv("POSTGRES_PORT"),
-		User:     os.Getenv("POSTGRES_USER"),
-		Password: os.Getenv("POSTGRES_PASSWORD"),
-		DbName:   os.Getenv("POSTGRES_DB"),
-	}
+// func ConnectPostgres() {
+// 	config := Config{
+// 		Host:     os.Getenv("POSTGRES_HOST"),
+// 		Port:     os.Getenv("POSTGRES_PORT"),
+// 		User:     os.Getenv("POSTGRES_USER"),
+// 		Password: os.Getenv("POSTGRES_PASSWORD"),
+// 		DbName:   os.Getenv("POSTGRES_DB"),
+// 	}
 
-	dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=disable", config.Host, config.User, config.Password, config.DbName, config.Port)
-	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
-	if err != nil {
-		panic(err)
-	}
+// 	dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=disable", config.Host, config.User, config.Password, config.DbName, config.Port)
+// 	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+// 	if err != nil {
+// 		panic(err)
+// 	}
 
-	Conn = db
-}
+// 	Conn = db
+// }
