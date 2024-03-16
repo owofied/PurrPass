d9af699177bd7a0a4a1b1ecdcdaa62c2afefc90f
Xiro <xiro@duck.com>
2024-03-16 02:16:57 +0100

    chore: code formatting

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,17 @@
 use axum::Router;
 use log::info;
 
+mod config;
+
 #[tokio::main]
-async fn main () {
+async fn main() {
     env_logger::init();
 
     let app = Router::new();
 
     info!("Starting server");
-    let listener = tokio::net::TcpListener::bind("127.0.0.1:3000").await.unwrap();
+    let listener = tokio::net::TcpListener::bind("127.0.0.1:3000")
+        .await
+        .unwrap();
     axum::serve(listener, app).await.unwrap()
 }
