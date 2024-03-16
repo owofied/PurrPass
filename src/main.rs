c2945d2501b3b330ab8641c531120e6fe3ed2f20
Xiro <xiro@duck.com>
2024-03-16 01:56:49 +0100

    feat: Simple axum server

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
-fn main() {
-    println!("Hello, world!");
+use axum::Router;
+use log::info;
+
+#[tokio::main]
+async fn main () {
+    env_logger::init();
+
+    let app = Router::new();
+
+    info!("Starting server");
+    let listener = tokio::net::TcpListener::bind("127.0.0.1:3000").await.unwrap();
+    axum::serve(listener, app).await.unwrap()
 }
