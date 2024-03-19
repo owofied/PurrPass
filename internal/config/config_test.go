524dc0245d9801009c0e9c6845bfe92bf59f8914
Xiro <xiro@duck.com>
2024-03-19 20:55:53 +0100

    refactor: Tests for GetConfig function

--- internal/config/config_test.go
+++ internal/config/config_test.go
@@ -1,6 +1,8 @@
+//nolint:goconst // Nuh uh
 package config_test
 
 import (
+	"encoding/json"
 	"errors"
 	"os"
 	"testing"
@@ -42,5 +44,49 @@ func TestInitializeConfig(t *testing.T) {
 	permissions = f.Mode().Perm()
 	if permissions != 0o600 {
 		t.Errorf("Config file permissions not set to 600\n")
+	}
+}
+
+func TestGetConfig(t *testing.T) {
+	tempDir := t.TempDir()
+
+	// Use a temporary directory for testing
+	// This variable is defined in config.go at line 40
+	config.UserConfigDirFunc = func() (string, error) {
+		return tempDir, nil
+	}
+
+	config.InitializeConfig()
+
+	cfg := config.GetConfig()
+	file, err := os.ReadFile(tempDir + "/PurrPass/config.json")
+	if err != nil {
+		t.Errorf("Failed to read config file at %s/PurrPass/config.json\n", tempDir)
+	}
+
+	var uMarshal *config.Config
+	err = json.Unmarshal(file, &uMarshal)
+	if err != nil {
+		t.Error("Failed to unmarshal config")
+	}
+
+	// I'm sure there is a better way to do this, but I'm too lazy lol
+	if cfg.Server.Port != uMarshal.Server.Port {
+		t.Errorf("Returned port %s doesn't match port in config file %s\n", cfg.Server.Port, uMarshal.Server.Port)
+	}
+	if cfg.Database.Host != uMarshal.Database.Host {
+		t.Errorf("Returned database host %s doesn't match database host in config file %s\n", cfg.Database.Host, uMarshal.Database.Host)
+	}
+	if cfg.Database.Port != uMarshal.Database.Port {
+		t.Errorf("Returned database port %s doesn't match database port in config file %s\n", cfg.Database.Port, uMarshal.Database.Port)
+	}
+	if cfg.Database.User != uMarshal.Database.User {
+		t.Errorf("Returned database user %s doesn't match database user in config file %s\n", cfg.Database.User, uMarshal.Database.User)
+	}
+	if cfg.Database.Password != uMarshal.Database.Password {
+		t.Errorf("Returned database password %s doesn't match database password in config file %s\n", cfg.Database.Password, uMarshal.Database.Password)
+	}
+	if cfg.Database.DBName != uMarshal.Database.DBName {
+		t.Errorf("Returned database name %s doesn't match database name in config file %s\n", cfg.Database.DBName, uMarshal.Database.DBName)
 	}
 }
